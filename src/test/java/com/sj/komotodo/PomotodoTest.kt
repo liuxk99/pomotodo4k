5c917651aa760dbf6319e9a2b14fdc31633144af
Thomas Liu <liuxk99@gmail.com>
2022-06-22 15:44:46 +0800

    [Dev<#F-feature>]: read token from file.

---
+++ src/test/java/com/sj/komotodo/PomotodoTest.kt
@@ -0,0 +1,29 @@
+package com.sj.komotodo
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import java.io.File
+
+fun readFileDirectlyAsText(fileName: String): String
+    = File(fileName).readText(Charsets.UTF_8)
+
+class PomotodoTest {
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+    }
+
+    @After
+    @Throws(Exception::class)
+    fun tearDown() {
+    }
+
+    @Test
+    fun testGetPomos() {
+        // You should store token in ${MODULE} root directory.
+        val token: String = readFileDirectlyAsText("pomotodo.token")
+        println(token)
+
+    }
+}
