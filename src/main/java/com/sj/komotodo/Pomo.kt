3d7043bc7c47afe22b3169376b2ffe8d2c5315d1
Thomas Liu <liuxk99@gmail.com>
2022-06-22 13:59:41 +0800

    [Dev<#F-feature>]: change String to Date and override toString().

--- src/main/java/com/sj/komotodo/Pomo.kt
+++ src/main/java/com/sj/komotodo/Pomo.kt
@@ -1,6 +1,7 @@
 package com.sj.komotodo
 
 import com.google.gson.annotations.SerializedName
+import java.util.*
 
 /*
 https://pomotodo.github.io/api-doc/#api-Pomo-GetPomo
@@ -20,16 +21,37 @@ HTTP/1.1 200 OK
   "manual": false
 }
  */
-data class Pomo (
-    @SerializedName("uuid") val uuid : String,
-    @SerializedName("created_at") val created_at : String,
-    @SerializedName("updated_at") val updated_at : String,
-    @SerializedName("description") val description : String = "",
-    @SerializedName("started_at") val started_at : String,
-    @SerializedName("ended_at") val ended_at : String,
-    @SerializedName("local_started_at") val local_started_at : String,
-    @SerializedName("local_ended_at") val local_ended_at : String,
-    @SerializedName("length") val length : Int,
-    @SerializedName("abandoned") val abandoned : Boolean = false,
-    @SerializedName("manual") val manual : Boolean = true
-)
+data class Pomo(
+    @SerializedName("uuid") val uuid: String,
+    @SerializedName("created_at") val created_at: Date,
+    @SerializedName("updated_at") val updated_at: Date,
+    @SerializedName("description") val description: String = "",
+    @SerializedName("started_at") val started_at: Date,
+    @SerializedName("ended_at") val ended_at: Date,
+    @SerializedName("local_started_at") val local_started_at: Date,
+    @SerializedName("local_ended_at") val local_ended_at: Date,
+    @SerializedName("length") val length: Int,
+    @SerializedName("abandoned") val abandoned: Boolean = false,
+    @SerializedName("manual") val manual: Boolean = true
+) {
+
+    override fun toString(): String {
+        val LF = "\n"
+        val sb = StringBuilder("[" + this.javaClass.name + "] {${LF}")
+        run {
+            sb.append(" uuid: ${uuid}${LF}")
+            sb.append(" created_at: ${created_at}${LF}")
+            sb.append(" updated_at: ${updated_at}${LF}")
+            sb.append(" description: ${description}${LF}")
+            sb.append(" started_at: ${started_at}${LF}")
+            sb.append(" ended_at: ${ended_at}${LF}")
+            sb.append(" local_started_at: ${local_started_at}${LF}")
+            sb.append(" local_ended_at: ${local_ended_at}${LF}")
+            sb.append(" length: ${length}${LF}")
+            sb.append(" abandoned: ${abandoned}${LF}")
+            sb.append(" manual: ${manual}${LF}")
+        }
+        sb.append("}").append(LF)
+        return sb.toString()
+    }
+}
