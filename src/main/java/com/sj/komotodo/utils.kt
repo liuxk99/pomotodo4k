d22b4dd0a68bba1c010423f85240e34c8db0fae7
Thomas Liu <liuxk99@gmail.com>
2022-06-22 18:40:51 +0800

    [Test<#F-feature>]: unittest for utils.

--- src/main/java/com/sj/komotodo/utils.kt
+++ src/main/java/com/sj/komotodo/utils.kt
@@ -1,6 +1,14 @@
 package com.sj.komotodo
 
 import java.io.File
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
-fun readFileDirectlyAsText(fileName: String): String
-    = File(fileName).readText(Charsets.UTF_8)
+fun readFileDirectlyAsText(fileName: String): String = File(fileName).readText(Charsets.UTF_8)
+
+fun Date.toISO8601(): String {
+    val iso8601SDF = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+    val iso8601Date: DateFormat = SimpleDateFormat(iso8601SDF, Locale.getDefault())
+    return iso8601Date.format(this)
+}
---
+++ src/test/java/com/sj/komotodo/UtilTest.kt
@@ -0,0 +1,20 @@
+package com.sj.komotodo
+
+import java.util.*
+
+class UtilTest {
+
+    @org.junit.Before
+    fun setUp() {
+    }
+
+    @org.junit.After
+    fun tearDown() {
+    }
+
+    @org.junit.Test
+    fun testDateToISO8601() {
+        val date = Date()
+        println(date.toISO8601())
+    }
+}
